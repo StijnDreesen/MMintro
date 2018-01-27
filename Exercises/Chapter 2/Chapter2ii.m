6be9a79eb4c17f303737e2a965e023abb9c40724
Stijn Dreesen <35848957+StijnDreesen@users.noreply.github.com>
2018-01-27 17:58:05 +0100

    Updated 27/01/18

--- Exercises/Chapter 2/Chapter2ii.m
+++ Exercises/Chapter 2/Chapter2ii.m
@@ -11,11 +11,11 @@ lb      = [0, 0];                                         % Lower bound.
 c       = NaN(reps, 2)                                    % Where optimized consumption will end up in.
 I       = 50                                              % Income is set to 50, so when the shock is applied, the income is U[50,100] (similar to the textbook).
 opts    = optimset('algorithm', 'sqp', 'display', 'off'); % Same options as ConsumptionSim.
-Inc     = NaN(reps, 1)
+Inc     = NaN(reps, 1)                                    % Allocate space to save the income variation in.
 
 for i = 1:reps                                            % For each individual (1 to 100).
     TempI   = I + Ishock(i, :);                           % Introduce the shock to the income.
-    Inc(i)  = TempI                                       % We save the income for later user in the Engel curves.
+    Inc(i)  = TempI                                       % We save the income for later use in the Engel curves.
     ub      = TempI./P                                    % Define the upper bound.
     c(i, :) = fmincon(@CobbDouglas, x0, P, TempI, [], [], lb, ub, [], opts)
 end
