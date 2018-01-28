4dd5a4e893b6dddb1f16713ad5a0eccfbbb95a3a
Stijn Dreesen <35848957+StijnDreesen@users.noreply.github.com>
2018-01-28 19:11:15 +0100

    Updated 28/01/18

--- Exercises/Chapter 3/Chapter3iib.m
+++ Exercises/Chapter 3/Chapter3iib.m
@@ -6,11 +6,6 @@ X = [ones(74,1) DataIn(:,2:3)];
 y = DataIn(:,1);
 [Beta,Q] = fminsearch(@(B) GMMObjective(B,y,X), [10,0,0]', optimset('TolX',1e-9))
 
-% If Beta is calculated, calculating the standard errors is
-% straightforward: 
+% Our weight matrix is not the optimal one. The covariance matrix is then:
 
-[N, K] = size(X)
-yhat = X*Beta;
-u = y - yhat;
-VarHat = inv(X'*X)*inv(N-K)*(u'*u);
-BetaSE = diag(sqrt(VarHat)) % Note that we only need the diagonal elements.
+% inv(G'WG)* (G'WVWG) * inv(G'WG)
