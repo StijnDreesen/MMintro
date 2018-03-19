20a5bf794290c4afe813924b759ae6bc9c60f681
Stijn Dreesen <stijn.dreesen@kuleuven.be>
2018-03-19 15:19:04 +0100

    Update Chapter3iv.m

--- Exercises/Chapter 3/Chapter3iv.m
+++ Exercises/Chapter 3/Chapter3iv.m
@@ -3,7 +3,7 @@
 % OLS can easily be viewed as an MM estimator, where rather than minimizing
 % the quadratic distance, we simply set each moment to zero instead.
 % Indeed, the sample moment should be equal to zero (and it is precisely of
-% the form in 3.4. Effectively, coding this estimator boils down to
+% the form in 3.4). Effectively, coding this estimator boils down to
 % stripping GMMobjective.m of the parts referring to quadratic distance.
 % Indeed, we can much more simply use the fsolve function to solve
 % MMobjective.m for Beta, setting m = 0 as in (3.4.). Notice the output of
